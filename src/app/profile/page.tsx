ce89f8103cc2925f1b40a011e09998322b8501d5
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-08 11:06:40 +0500

    add: added profile info form in ui design

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,8 @@
 'use server';
-// import ProfileInfoForm from "@/components/ProfileInfoForm";
+import ProfileInfoForm from "@/components/ProfileInfoForm";
 import {authOptions} from "@/lib/authOptions";
 // import {Donation, DonationModel} from "@/models/Donation";
-// import {ProfileInfoModel} from "@/models/ProfileInfo";
+import {ProfileInfoModel} from "@/models/ProfileInfo";
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import mongoose from "mongoose";
@@ -16,16 +16,16 @@ export default async function ProfilePage() {
 
   const email = session.user.email;
   await mongoose.connect(process.env.MONGODB_URI as string);
-//   const profileInfoDoc = JSON.parse(JSON.stringify(
-//     await ProfileInfoModel.findOne({email})
-//   ));
+  const profileInfoDoc = JSON.parse(JSON.stringify(
+    await ProfileInfoModel.findOne({email})
+  ));
 
 //   const donations:Donation[] = await DonationModel.find({paid:true, email});
 //   const total = donations.reduce((current, d) => current + d.amount * 5, 0);
 
   return (
     <div className="max-w-2xl mx-auto px-4 mt-4">
-      {/* <ProfileInfoForm profileInfo={profileInfoDoc}/> */}
+      <ProfileInfoForm profileInfo={profileInfoDoc}/>
       <div className="bg-yellow-300/20 border-2 border-yellow-300 p-4 rounded-xl flex items-center gap-2 my-4 justify-between">
         <div className="flex items-center gap-2">
           {/* Total money received: <span className="text-2xl">${total}</span> */}
